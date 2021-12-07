cdeabcb45a6413968fbfa94541a886309709c0d4
teddywashere <76180189+teddywashere@users.noreply.github.com>
2021-12-07 20:02:56 +0100

    Add files via upload

---
+++ dbInit.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+require('./DatabaseModels/Warns')(sequelize, Sequelize.DataTypes);
+
+const force = process.argv.includes('--force') || process.argv.includes('-f');
+
+sequelize.sync({ force }).then(async () => {
+	console.log('Database synced');
+
+	sequelize.close();
+}).catch(console.error);
