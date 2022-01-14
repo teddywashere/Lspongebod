4814e1f0b1cdd285e32629ee19801ff69c10e486
teddywashere <76180189+teddywashere@users.noreply.github.com>
2022-01-14 11:30:14 +0100

    Add files via upload

--- Commands/Ncommands/tickets/supportticket.js
+++ Commands/Ncommands/tickets/supportticket.js
@@ -1,5 +1,6 @@
 const { Permissions } = require('discord.js');
 const Discord = require('discord.js');
+
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
 	host: 'localhost',
@@ -12,43 +13,43 @@ const Setup = require('../../../DatabaseModels/Setup')(sequelize, Sequelize);
 const Ticket = require('../../../DatabaseModels/Tickets')(sequelize, Sequelize);
 
 module.exports = {
-	name: 'report',
-	description: 'Opens report ticket',
+	name: 'support',
+	description: 'Opens support ticket',
 	guildOnly: true,
 	async execute(message) {
 		try {
-			const logger = '291194093495648258';
+			// 298822483060981760
+			const logger = '905155961180794920';
 			const server = await Setup.findOne({ where: { guild_id: message.guild.id } });
-			if (!server) return message.reply({ content: `Please do /setup error first` });
+			if (!server) return interaction.reply({ content: `Please do /setup error first` });
+	
 			const member = await message.guild.members.cache.get(message.author.id);
-			if (!member) return message.reply('Member not found');
 			const logs = await message.guild.channels.cache.get(server.logs_channel);
-			const category = await message.guild.channels.cache.get(server.opentickets_channel);
-			const tickets = await message.guild.channels.cache.get(server.ticket_channel);
-			const staff = await message.guild.roles.cache.get(server.staff_role);
-			if (server.jail_role != '0') {
-				if (!message.guild.roles.cache.get(server.jail_role)) return message.reply({ content: `Jail role not found`, ephemeral: true });
-				if (member.roles.cache.has(server.jail_role)) return message.reply({ content: `You're in jail, you can only verify.` });
-			}
-			if (!message.guild.roles.cache.get(server.everyone_role)) return message.reply({ content: `Everyone role not found`, ephemeral: true });
 
-			
-			if (tickets) {
+			const category = await message.guild.channels.cache.get(server.ticketopen_channel);
+			const ticketc = await message.guild.channels.cache.get(server.tickets_channel);
+			const staff = await message.guild.roles.cache.get(server.staff_role);;
+			if (!message.guild.roles.cache.get(server.everyone_role)) return message.reply({ content: 'Everyone role not found' });
+
+			if(message.guild.roles.cache.get(server.jail_role)) {
+				if (member.roles.cache.has(server.jail_role)) return message.reply({ content: `You're in jail so you can only verify` });
+			}
+
+			if (ticketc) {
 				if (message.channel != ticketc) return message.reply({ content: `Please only use this command in ${ticketc}.\n If you try to do this again, moderators will mute you.` });
 			}
 
-			const channel = await Ticket.findOne({ where: { user_id: message.author.id,  type: 'report', guild_id: message.guild.id, status: 'open' } } );
-			const look = await message.guild.channel.cache.get(channel.channel_id);
-			if (channel) return message.reply(`You already have an open report ticket!\n${look}`);
+			const ticket = await Ticket.findOne({ where: { user_id: message.author.id, guild_id: message.guild.id, status: 'open', type: 'suggestion' } });
+			if (ticket) return message.reply({ content: `You already have an open ticket ${message.guild.channels.cache.find(ticket.channel_id)}`});
 
-			const all = await Ticket.findAll({ where: { guild_id: message.guild.id, status: 'open', type: 'report' } });
+			const all = await Ticket.findAll({ where: { guild_id: message.guild.id, status: 'open', type: 'support' } });
 			const array = await all.map(n => n.number);
-			const number = array.lenght + 1;
+			const number = array.length + 1;
 
 			if (server.ticket_name === 'username') {
 				if (category) {
 					if (staff) {
-						const make = await message.guild.channels.create(`report-${message.author.tag}`, {
+						const make = await message.guild.channels.create(`support-${message.author.tag}`, {
 							parent: category,
 							permissionOverwrites: [
 								{
@@ -69,18 +70,19 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.author} ${staff}`)
+						await make.send(`${message.author} ${staff}`)
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
+							
 						})
 					}
 					if (!staff) {
-						const make = await message.guild.channels.create(`report-${message.author.tag}`, {
+						const make = await message.guild.channels.create(`support-${message.author.tag}`, {
 							parent: category,
 							permissionOverwrites: [
 								{
@@ -97,20 +99,20 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						await make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 				}
 				if (!category) {
 					if (staff) {
-						const make = await message.guild.channels.create(`report-${message.author.tag}`, {
+						const make = await message.guild.channels.create(`support-${message.author.tag}`, {
 							permissionOverwrites: [
 								{
 									id: message.author.id,
@@ -130,18 +132,18 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${member} ${staff}`);
+						await make.send(`${member} ${staff}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 					if (!staff) {
-						const make = await message.guild.channels.create(`report-${message.author.tag}`, {
+						const make = await message.guild.channels.create(`support-${message.author.tag}`, {
 							permissionOverwrites: [
 								{
 									id: message.author.id,
@@ -157,32 +159,31 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						await make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 				}
-				const made = message.guild.channels.find(n => n === `report${message.author.tag}`)
-					const reportembed = new Discord.MessageEmbed()
-				.setTitle(`:oncoming_police_car:**Report ticket opened**:oncoming_police_car:`)
+				const made = message.guild.channels.find(n => n === `support${message.author.tag}`)
+				const verifyembed = new Discord.MessageEmbed()
+				.setTitle(`:incoming_envelope:**support ticket opened**:incoming_envelope:`)
 				.setDescription(`_ _\n**Member:** ${message.author}\n\n **Ticket:** ${made}\n_ _`)
-				.setColor('#ff610c')
-				.setThumbnail('https://cdn.discordapp.com/attachments/772471934231117834/912711229955526727/Screenshot_20211123-152807_1.png')
+				.setColor('#00d1dc')
+				.setThumbnail('https://cdn.discordapp.com/attachments/772471934231117834/912711230391722064/Screenshot_20211123-152732_1.png')
 				.setTimestamp();
 
-					await logs.send({ embeds: [reportembed] }).catch(O_o => {});
+			await logs.send({ embeds: [verifyembed] }).catch(O_o => {});
 			}
 			if (server.ticket_name === 'number') {
-
 				if (category) {
 					if (staff) {
-						const make = await message.guild.channels.create(`report-${number}`, {
+						const make = await message.guild.channels.create(`support-${number}`, {
 							parent: category,
 							permissionOverwrites: [
 								{
@@ -203,18 +204,18 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.author} ${staff}`)
+						await make.send(`${message.author} ${staff}`)
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 					if (!staff) {
-						const make = await message.guild.channels.create(`report-${number}`, {
+						const make = await message.guild.channels.create(`support-${number}`, {
 							parent: category,
 							permissionOverwrites: [
 								{
@@ -231,20 +232,21 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						await make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 				}
 				if (!category) {
 					if (staff) {
-						const make = await message.guild.channels.create(`report-${number}`, {
+						const make = await message.guild.channels.create(`support-${number}`, {
+							parent: category,
 							permissionOverwrites: [
 								{
 									id: message.author.id,
@@ -264,18 +266,18 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${member} ${staff}`);
+						await make.send(`${member} ${staff}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 					if (!staff) {
-						const make = await message.guild.channels.create(`report-${number}`, {
+						const make = await message.guild.channels.create(`support-${number}`, {
 							permissionOverwrites: [
 								{
 									id: message.author.id,
@@ -291,29 +293,31 @@ module.exports = {
 								},
 							],
 						});
-						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						await make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
 						return Ticket.create({
 							user_id: message.author.id,
 							channel_id: make.id,
 							guild_id: message.guild.id,
 							status: 'open',
-							type: 'report',
+							type: 'support',
 							number: number,
 						})
 					}
 				}
-				const made = message.guild.channels.find(n => n === `report${number}`)
-					const reportembed = new Discord.MessageEmbed()
-				.setTitle(`:oncoming_police_car:**Report ticket opened**:oncoming_police_car:`)
+				const made = message.guild.channels.find(n => n === `support${number}`)
+				const verifyembed = new Discord.MessageEmbed()
+				.setTitle(`:incoming_envelope:**support ticket opened**:incoming_envelope:`)
 				.setDescription(`_ _\n**Member:** ${message.author}\n\n **Ticket:** ${made}\n_ _`)
-				.setColor('#ff610c')
-				.setThumbnail('https://cdn.discordapp.com/attachments/772471934231117834/912711229955526727/Screenshot_20211123-152807_1.png')
+				.setColor('#00d1dc')
+				.setThumbnail('https://cdn.discordapp.com/attachments/772471934231117834/912711230391722064/Screenshot_20211123-152732_1.png')
 				.setTimestamp();
+
+			await logs.send({ embeds: [verifyembed] }).catch(O_o => {});
+
 
 					await logs.send({ embeds: [reportembed] }).catch(O_o => {});
 			}
-
-			return message.reply({ content: `Your report ticket has been created!` });
+			return message.reply({ content: `Your support ticket has been created!` });
 		}
 		catch (error) {
 			console.error(error);
--- Commands/Ncommands/tickets/verifyticket.js
+++ Commands/Ncommands/tickets/verifyticket.js
@@ -1,67 +1,312 @@
 const { Permissions } = require('discord.js');
 const Discord = require('discord.js');
 
-const { logsc, opentickets, jailr, staffr, everyoner, logger, owner } = require('../../../config.json');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+const Setup = require('../../../DatabaseModels/Setup')(sequelize, Sequelize);
+const Ticket = require('../../../DatabaseModels/Tickets')(sequelize, Sequelize);
 
 module.exports = {
 	name: 'verify',
 	description: 'Opens age verification ticket',
 	async execute(message) {
 		try {
+			const logger = '298822483060981760';
+			const server = Setup.findOne({ where: { guild_id: message.guild.id } });
+			if (!server) return message.reply({ content: `Please do /setup error first` });
 			const member = await message.guild.members.cache.get(message.author.id);
-			const logs = await message.guild.channels.cache.get(logsc);
-			const category = await message.guild.channels.cache.get(opentickets);
-			const staff = await message.guild.roles.cache.get(staffr);
+			const logs = await message.guild.channels.cache.get(server.logs_channel);
+			const category = await message.guild.channels.cache.get(server.opentickets_channel);
+			const staff = await message.guild.roles.cache.get(server.staff_role);
+			if (!message.guild.roles.cache.get(server.everyone_role)) return message.reply({ content: 'Everyone role not found' });
 
-			const me = await message.guild.members.cache.get(owner);
-			if (!logs) await me.send(`${message.author} tried to use verify ticket command in ${message.guild}\n**ERROR:**\nLogs channel not found`).catch(O_o => console.log(O_o));
+			if (message.guild.roles.cache.get(server.jail_role)) {
+				if (!member.roles.cache.has(server.jail_role)) return message.reply({ content: `You're not in jail so you don't need to verify` });
+			};
 
-			if (!message.guild.roles.cache.get(jailr)) return message.reply({ content: `Jail role not found` });
-			if (!staff) return message.reply({ content: `Staff role not found` });
-			if (!category) return message.reply({ content: 'Open tickets category not found' });
-			if (!message.guild.roles.cache.get(everyoner)) return message.reply({ content: 'Everyone role not found' });
-			if (!member.roles.cache.has(jailr)) return message.reply({ content: `You're not in jail so you don't need to verify` });
+			const ticket = Ticket.findOne({where: { user_id: message.author.id, guild_id: message.guild.id, status: 'open', type: 'verify' } });
+			if (ticket) {
+				return message.reply({ content: `You already have an open verification ticket: ${message.guild.channels.cache.get(ticket.channel_id)}` });
+			}
 
-			// !!!THE OPEN TICKET CHECK SEEMS TO BE BROKEN?
-			const name = await message.author.username.toLowerCase();
-			const disc = await message.author.discriminator.toString();
-			const channel = await message.guild.channels.cache.find(c => c.name === `verify-${name}${disc}`);
-			if (channel && channel.parentId === opentickets) return message.reply(`You already have an open verification ticket!\n${channel}`);
-
+			const all = await Ticket.findAll({ where: { guild_id: message.guild.id, status: 'open', type: 'verify' } });
+			const array = await all.map(n => n.number);
+			const number = array.lenght + 1;
 			// make private ticket
-			await message.guild.channels.create(`verify - ${message.author.tag}`, {
-				parent: category,
-				permissionOverwrites: [
-					{
-						id: message.author,
-						allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
-					},
-					{
-						id: staffr,
-						allow: [Permissions.FLAGS.VIEW_CHANNEL],
-					},
-					{
-						id: logger,
-						allow: [Permissions.FLAGS.VIEW_CHANNEL],
-					},
-					{
-						id: everyoner,
-						deny: [Permissions.FLAGS.VIEW_CHANNEL],
-					},
-				],
-			}).then(c => c.send(`${member} ${staff}`));
-
-			// send logs
-			const ticket = await message.guild.channels.cache.find(c => c.name === `suggestion-${name}${disc}`);
-			const verifyembed = new Discord.MessageEmbed()
+			if (server.ticket_name === 'username') {
+				if (category) {
+					if (staff) {
+						const make = await message.guild.channels.create(`verify-${message.author.tag}`, {
+							parent: category,
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: server.staff_role,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.author} ${staff}`)
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+							
+						})
+					}
+					if (!staff) {
+						const make = await message.guild.channels.create(`verify-${message.author.tag}`, {
+							parent: category,
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+				}
+				if (!category) {
+					if (staff) {
+						const make = await message.guild.channels.create(`verify-${message.author.tag}`, {
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: server.staff_role,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${member} ${staff}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+					if (!staff) {
+						const make = await message.guild.channels.create(`verify-${message.author.tag}`, {
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+				}
+				const made = message.guild.channels.find(n => n === `verify${message.author.tag}`)
+				const verifyembed = new Discord.MessageEmbed()
 				.setTitle(`:shield:**Age verification ticket opened**:shield:`)
-				.setDescription(`_ _\n**Member:** ${message.author}\n\n **Ticket:** ${ticket}\n_ _`)
+				.setDescription(`_ _\n**Member:** ${message.author}\n\n **Ticket:** ${made}\n_ _`)
 				.setColor('#ffb81d')
 				.setThumbnail('https://media.discordapp.net/attachments/772471934231117834/911718877988286464/3d8.jpg?width=661&height=661')
 				.setTimestamp();
 
-			await logs.send({ embeds: [verifyembed] }).catch(O_o => me.send(`${message.author} tried to use verify ticket command in ${message.guild}\n**ERROR:**\n${O_o}`));
+			await logs.send({ embeds: [verifyembed] }).catch(O_o => {});
+			}
+			if (server.ticket_name === 'number') {
+				if (category) {
+					if (staff) {
+						const make = await message.guild.channels.create(`verify-${number}`, {
+							parent: category,
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: server.staff_role,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.author} ${staff}`)
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+							
+						})
+					}
+					if (!staff) {
+						const make = await message.guild.channels.create(`verify-${number}`, {
+							parent: category,
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+				}
+				if (!category) {
+					if (staff) {
+						const make = await message.guild.channels.create(`verify-${number}`, {
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: server.staff_role,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${member} ${staff}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+					if (!staff) {
+						const make = await message.guild.channels.create(`verify-${number}`, {
+							permissionOverwrites: [
+								{
+									id: message.author.id,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.EMBED_LINKS, Permissions.FLAGS.ATTACH_FILES, Permissions.FLAGS.READ_MESSAGE_HISTORY],
+								},
+								{
+									id: logger,
+									allow: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+								{
+									id: server.everyone_role,
+									deny: [Permissions.FLAGS.VIEW_CHANNEL],
+								},
+							],
+						});
+						make.send(`${message.guild.roles.cache.get(server.everyone_role)}`);
+						return Ticket.create({
+							user_id: message.author.id,
+							channel_id: make.id,
+							guild_id: message.guild.id,
+							status: 'open',
+							type: 'verify',
+							number: number,
+						})
+					}
+				}
+				const made = message.guild.channels.find(n => n === `verify${number}`)
+				const verifyembed = new Discord.MessageEmbed()
+				.setTitle(`:shield:**Age verification ticket opened**:shield:`)
+				.setDescription(`_ _\n**Member:** ${message.author}\n\n **Ticket:** ${made}\n_ _`)
+				.setColor('#ffb81d')
+				.setThumbnail('https://media.discordapp.net/attachments/772471934231117834/911718877988286464/3d8.jpg?width=661&height=661')
+				.setTimestamp();
 
+			await logs.send({ embeds: [verifyembed] }).catch(O_o => {});
+			}
 			return message.reply({ content: `Your verification ticket has been created!` });
 		}
 		catch (error) {
