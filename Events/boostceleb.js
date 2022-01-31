1e168019965f00390b216e8d829c45b07b44b41d
teddywashere <76180189+teddywashere@users.noreply.github.com>
2022-01-31 16:03:41 +0100

    Update boostceleb.js

--- Events/boostceleb.js
+++ Events/boostceleb.js
@@ -2,16 +2,12 @@ module.exports = {
     name: 'guildMemberUpdate',
 	once: true,
 	async execute(oldMember, newMember) {
-        if (newMember.guild.id != '788794708087275550') return console.log('the guild');
-		const hadRole = await oldMember.roles.cache.get('825842956249595905');
-        const hasRole = await newMember.roles.cache.get('825842956249595905');
-  
-// server booster role: 778982289878679592
-// server id: 634117134913503244
-// channel id: 814231770852622408
+        if (newMember.guild.id != '634117134913503244') return console.log('the guild');
+		const hadRole = await oldMember.roles.cache.get('778982289878679592');
+        const hasRole = await newMember.roles.cache.get('778982289878679592');
 
         if (!hadRole && hasRole) {
-            const channel = await newMember.guild.channels.cache.get('825026451102629949');
+            const channel = await newMember.guild.channels.cache.get('814231770852622408');
             const rainbow = await newMember.guild.members.cache.get('759085690360954890');
             await channel.send(`<a:xxs_kleehype:936694317051949097>`);
             await channel.send(`${newMember} Thank you for supporting us! <a:xy_giflove:936925217459154974>`);
